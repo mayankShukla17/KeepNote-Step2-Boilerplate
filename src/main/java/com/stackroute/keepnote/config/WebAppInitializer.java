2e9cbd80f38dc4e745beb792b5ee64653739b5c7
Mayank Shukla <mayankgonda@gmail.com>
2020-10-27 21:36:11 +0000

    KeepNote Step 2 Assignment Completed

--- src/main/java/com/stackroute/keepnote/config/WebAppInitializer.java
+++ src/main/java/com/stackroute/keepnote/config/WebAppInitializer.java
@@ -14,22 +14,16 @@ public class WebAppInitializer extends AbstractAnnotationConfigDispatcherServlet
 
 	@Override
 	protected Class<?>[] getRootConfigClasses() {
-	
-		return new Class[] {};
+		return new Class[] {ApplicationContextConfig.class};
 	}
 
 	@Override
 	protected Class<?>[] getServletConfigClasses() {
-	
-		return new Class[] {};
+		return new Class[] {WebMvcConfig.class};
 	}
 
 	@Override
 	protected String[] getServletMappings() {
-		
 		return new String[] {"/"};
 	}
-
-	
-
 }
--- src/main/java/com/stackroute/keepnote/model/Note.java
+++ src/main/java/com/stackroute/keepnote/model/Note.java
@@ -2,60 +2,103 @@ package com.stackroute.keepnote.model;
 
 import java.time.LocalDateTime;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
 /*
  * The class "Note" will be acting as the data model for the note Table in the database. Please
  * note that this class is annotated with @Entity annotation. Hibernate will scan all package for 
  * any Java objects annotated with the @Entity annotation. If it finds any, then it will begin the 
  * process of looking through that particular Java object to recreate it as a table in your database.
  */
-
+@Entity
 public class Note {
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private int noteId;
+	@Column(nullable = false)
+	private String noteTitle;
+	@Column(nullable = false)
+	private String noteContent;
+	@Column(nullable = false)
+	private String noteStatus;
+	private LocalDateTime createdAt;
 
 	public Note() {
 
 	}
 
-	public Note(int i, String string, String string2, String string3, LocalDateTime localDate) {
+	public Note(int noteId, String noteTitle, String noteContent, String noteStatus, LocalDateTime createdAt) {
+		super();
+		this.noteId = noteId;
+		this.noteTitle = noteTitle;
+		this.noteContent = noteContent;
+		this.noteStatus = noteStatus;
+		this.createdAt = createdAt;
 	}
 
 	public int getNoteId() {
+		return noteId;
+	}
 
-		return 0;
+	public void setNoteId(int noteId) {
+		this.noteId = noteId;
 	}
 
 	public String getNoteTitle() {
+		return noteTitle;
+	}
 
-		return null;
+	public void setNoteTitle(String noteTitle) {
+		this.noteTitle = noteTitle;
 	}
 
 	public String getNoteContent() {
+		return noteContent;
+	}
 
-		return null;
+	public void setNoteContent(String noteContent) {
+		this.noteContent = noteContent;
 	}
 
 	public String getNoteStatus() {
-
-		return null;
+		return noteStatus;
 	}
 
-	public void setNoteId(int parseInt) {
-
+	public void setNoteStatus(String noteStatus) {
+		this.noteStatus = noteStatus;
 	}
 
-	public void setNoteTitle(String parameter) {
-
+	public LocalDateTime getCreatedAt() {
+		return createdAt;
 	}
 
-	public void setNoteContent(String parameter) {
-
+	public void setCreatedAt(LocalDateTime createdAt) {
+		this.createdAt = createdAt;
 	}
 
-	public void setNoteStatus(String parameter) {
-
+	@Override
+	public boolean equals (Object o) {
+		if (!(o instanceof Note)) {
+			return false;
+		}
+		Note note = (Note)o;
+		return getNoteId()==note.getNoteId() &&
+				getNoteTitle().equals(note.getNoteTitle()) &&
+				getNoteContent().equals(note.getNoteContent())&&
+				getNoteStatus().equals(note.getNoteStatus());
 	}
 
-	public void setCreatedAt(LocalDateTime now) {
-
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result=1;
+		result = prime * result + Integer.toString(getNoteId()).hashCode();
+		result = prime * result + getNoteTitle().hashCode();
+		result = prime * result + getNoteContent().hashCode();
+		return result;
 	}
-
 }
